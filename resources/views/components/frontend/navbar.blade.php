10f892f09fad9e83e8b734f5ca33b39d958f0755
Ochui Princewill <ochuiprincewill411@gmail.com>
2021-07-30 19:12:09 +0100

    fix nav

--- resources/views/components/frontend/navbar.blade.php
+++ resources/views/components/frontend/navbar.blade.php
@@ -15,17 +15,14 @@
                                 <a class="nav-link" href="/" id="homeDropdown" role="button">Home</a>
                             </li>
                             <li class="nav-item dropdown menu_list">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarExplore" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Find a Realtor
-                </a>
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarExplore" role="button" data-bs-toggle="dropdown" aria-expanded="false">Explore</a>
                                 <div class="dropdown-menu w-100 p-0" aria-labelledby="navbarExplore">
                                     <div class="row g-0">
                                         <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 d-xl-block d-lg-block d-md-none d-none">
                                             <div class="dropdown-img " style="background-image: url({{asset('images/dropdown-img.jpg') }});">
                                                 <div class="dropdown-img-caption">
-                                                    <h2 class="text-white mb-1">Want Experience on Rentkit ?</h2>
-                                                    <p class="mb-4">Experience a level of our quality in both design &amp; customization works.
-                                                    </p>
+                                                    <h2 class="text-white mb-1">Get listed </h2>
+                                                    <p class="mb-4">Take the perceived professionalism of your business to the next level and maximise your business's potential by joining us.</p>
                                                     <a href="add-listing.html" class="btn btn-primary">
                             Join Us
                           </a>
@@ -35,93 +32,39 @@
                                         <div class="col-12 col-md-12 col-lg-6">
                                             <div class="row m-3 m-lg-6">
                                                 <div class="col-md-3">
-                                                    <h5 class="mb-2 text-uppercase h6 text-primary fw-bold">Listing</h5>
+                                                    <h5 class="mb-2 text-uppercase h6 text-primary fw-bold">Inspector</h5>
                                                     <ul class="list-unstyled">
-                                                        <li><a class="dropdown-item" href="listing-list-view.html">
-                                Listing View</a>
-                                                        </li>
-                                                        <li><a class="dropdown-item" href="listing-grid-view.html">
-                                Grid View</a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="dropdown-item" href="list-with-map.html">
-                                List With Map</a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="dropdown-item" href="grid-with-map.html">
-                                Grid With Map</a>
-                                                        </li>
-                                                        <li><a class="dropdown-item" href="listing-category.html">
-                                Listing Category</a>
-                                                        </li>
-                                                        <li><a class="dropdown-item" href="list-single.html">
-                                Listing Single</a>
-                                                        </li>
-                                                        <li><a class="dropdown-item" href="add-listing.html">
-                                Add Listing</a>
-                                                        </li>
+                                                        <li><a class="dropdown-item" href="listing-list-view.html">Top Rated</a></li>
+                                                        <li><a class="dropdown-item" href="listing-grid-view.html">Featured</a></li>
+                                                        <li><a class="dropdown-item" href="list-with-map.html">Popular</a></li>
+                                                        
 
                                                     </ul>
                                                 </div>
 
                                                 <div class="col-md-3">
-                                                    <h5 class="mb-2 text-uppercase h6 text-primary fw-bold">Other Pages</h5>
+                                                    <h5 class="mb-2 text-uppercase h6 text-primary fw-bold">Appraisers</h5>
                                                     <ul class="mega-menu-list list-unstyled">
-                                                        <li><a class="dropdown-item" href="about.html">
-                              About us</a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="dropdown-item" href="contact-us.html">
-                              Contact</a>
-                                                        </li>
-
-                                                        <li><a class="dropdown-item" href="faq.html">
-                                Faq</a>
-                                                        </li>
-                                                        <li><a class="dropdown-item" href="pricing.html">
-                                Pricing</a>
-                                                        </li>
-                                                        <li><a class="dropdown-item" href="checkout-page.html">
-                              Checkout</a>
-                                                        </li>
-
-                                                        <li><a class="dropdown-item" href="coming-soon.html">
-                              Coming Soon</a>
-                                                        </li>
-                                                        <li><a class="dropdown-item" href="404-error.html">
-                                404 Error</a>
-                                                        </li>
+                                                        <li><a class="dropdown-item" href="listing-list-view.html">Top Rated</a></li>
+                                                        <li><a class="dropdown-item" href="listing-grid-view.html">Featured</a></li>
+                                                        <li><a class="dropdown-item" href="list-with-map.html">Popular</a></li>
                                                     </ul>
                                                 </div>
                                                 <div class="col-md-3">
-                                                    <h5 class="mb-2 text-uppercase h6 text-primary fw-bold">Author</h5>
+                                                    <h5 class="mb-2 text-uppercase h6 text-primary fw-bold">Realtors</h5>
                                                     <ul class="list-unstyled">
-                                                        <li><a class="dropdown-item" href="author-list.html">
-                                List</a>
-                                                        </li>
-                                                        <li><a class="dropdown-item" href="author-reviews.html">
-                                Reviews</a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="dropdown-item" href="author-favourite.html">
-                                Favourite</a>
-                                                        </li>
+                                                        <li><a class="dropdown-item" href="listing-list-view.html">Top Rated</a></li>
+                                                        <li><a class="dropdown-item" href="listing-grid-view.html">Featured</a></li>
+                                                        <li><a class="dropdown-item" href="list-with-map.html">Popular</a></li>
 
                                                     </ul>
                                                 </div>
                                                 <div class="col-md-3">
-                                                    <h5 class="mb-2 text-uppercase h6 text-primary fw-bold">Account</h5>
+                                                    <h5 class="mb-2 text-uppercase h6 text-primary fw-bold">Attornies</h5>
                                                     <ul class="mega-menu-list list-unstyled">
-                                                        <li><a class="dropdown-item" href="signin.html">
-                                Login</a>
-                                                        </li>
-                                                        <li><a class="dropdown-item" href="signup.html">
-                                Register</a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="dropdown-item" href="forget-password.html">
-                                Forgot Password</a>
-                                                        </li>
+                                                        <li><a class="dropdown-item" href="listing-list-view.html">Top Rated</a></li>
+                                                        <li><a class="dropdown-item" href="listing-grid-view.html">Featured</a></li>
+                                                        <li><a class="dropdown-item" href="list-with-map.html">Popular</a></li>
                                                     </ul>
                                                 </div>
                                             </div>
